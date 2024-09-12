330f5d4eddbd1a6cb3c911a516b81343186e5ca9
leticianb <leticianakahara10@gmail.com>
2024-09-12 00:26:56 -0300

    Começando a integração com a API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log('Sucesso')
+const offset = 0;
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url).then(function(response) { //try, fetch sempre usa get
+        console.log(response)
+    })
+    .catch(function(error) { //catch(error)
+        console.log(error)
+    })
+    .finally(function() {
+        console.log('Requisição concluída') //finally
+    })
