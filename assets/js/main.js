7b21763d0bbaed6bdd0614ed65d48d860306cb0a
leticianb <leticianakahara10@gmail.com>
2024-09-17 17:07:57 -0300

    Finalizando o projeto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,46 @@
-// const limit = 10
-// const offset = 0;
-// const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
 
-// function convertPokemonsTypesToLi(pokemonTypes) {
-//     return pokemonTypes.map((typeSlot) => `<li class="type">${type}</li>`)
-// }
+const maxRecords = 151
+const limit = 10
+let offset = 0;
 
 function convertPokemonToLi(pokemon) {
-    return ` <li class="pokemon">
-                        <span class="number">#${pokemon.number}</span>
-                        <span class="name">${pokemon.name}</span>
-                        <div class="detail">
-                            <ul class="types">
-                            ${pokemon.types.map((type)=> `<li class="type">${type}</li>`).join('')}
-                            </ul>
-                            <img src="${pokemon.photo}"></div>`
+    return `
+        <li class="pokemon ${pokemon.type[0]}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"alt="${pokemon.name}">
+            </div>
+        </li>
+    `
 }
 
-const pokemonList = document.getElementById('pokemonList')
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
 
+loadPokemonItens(offset, limit)
 
-pokeApi.getPokemons().then((pokemons = []) => { //try, fetch sempre usa get, por padrao fetch sempre retorna string
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
 
-    // const newHtml = newList.join('')
-    // pokemonList.innerHTML += newHtml
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
 
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
 })
-
-
-// .catch((error) => //catch(error)
-//     console.log(error)
-// )
-// .finally(function() {
-//     console.log('Requisição concluída') //finally
-// })
